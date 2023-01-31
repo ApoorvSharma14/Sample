707e915aad632709bf87b0977776148af9efa5f3
ApoorvSharma14 <apoorv03sharma@gmail.com>
2023-01-31 14:32:07 +0530

    New Brach change

--- Hello.java
+++ Hello.java
@@ -3,7 +3,6 @@ public class Hello
     public static void main(Sting args[])
     {
         //Printing Hello World
-        System.out.println("Hello World");
-        System.out.println("Changes");
+        System.out.println("Hello");
     }
 }

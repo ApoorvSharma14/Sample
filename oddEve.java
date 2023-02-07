07ed2246d6be8950acd53a2bf062ed807efc0943
ApoorvSharma14 <apoorv03sharma@gmail.com>
2023-02-07 13:52:45 +0530

    ODD EVEN CODE

---
+++ oddEve.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class oddEve
+{
+    public static void main(String args[])
+    {
+        int n=in.nextInt();
+        if(n%2==0)
+        System.out.println("Even");
+        else
+        System.out.println("Odd");
+    }
+}
